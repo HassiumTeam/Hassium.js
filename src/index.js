da166cc44d1006498444d564b48a7c4746fb0aa9
Jacob Misirian <misirianrule@gmail.com>
2019-08-10 14:00:01 -0500

    Added exports to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const compile = require('./hassiumCompiler');
 const VM = require('./runtime/vm');
 
-let file = process.argv[2];
+if (process.argv.length > 2) {
+    let file = process.argv[2];
 
-if (file == "repl") {
-    require('./repl').run();
-} else {
-    let mod = compile({ file });
+    if (file == "repl") {
+        require('./repl').run();
+    } else {
+        let mod = compile({ file });
 
-    let vm = new VM(mod);
-    vm.run(mod);
+        let vm = new VM(mod);
+        vm.run(mod);
+    }
 }
+
+module.exports.compile = compile;
+module.exports.VM = VM;
